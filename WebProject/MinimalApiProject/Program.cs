be733c61a3e0efde40283dc2500ec3f68f2e60a4
makzs <erikmakiak@gmail.com>
2024-08-29 11:32:47 -0300

    teste2

--- WebProject/MinimalApiProject/Program.cs
+++ WebProject/MinimalApiProject/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! em C#");
+
+app.MapGet("/segundo", () => "Segunda Funcionalidade");
+
+app.MapGet("/retorno", () =>
+{
+    dynamic endereco = new
+    {
+        rua = "Praça Osorio",
+        numero = 125
+    };
+    return endereco;
+});
+
+//Criar novas funcionalidades/Endpoints para receber dados
+// Pela URL da requisição
+// Corpo da requisição
+// Guardar as informações em uma lista
 
 app.Run();
